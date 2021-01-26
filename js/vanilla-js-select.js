3af1eb03b4414def52320c7f4dba3d8fba099118
Web Time <>
2021-01-26 11:21:08 +0200

    add images function

--- js/vanilla-js-select.js
+++ js/vanilla-js-select.js
@@ -2,20 +2,44 @@ const CustomSelect = function (e) {
 
    let mainInitId = e.elem ? document.getElementById(e.elem) : e.elem,
       optgroup = mainInitId.querySelectorAll('optgroup'),
-      // s = "boolean" == typeof e.bubbles,
       selectOption = mainInitId.options,
+      selectedIndex = selectOption[mainInitId.selectedIndex],
       dataIndexCount = 0,
+      dataImgCount = 0,
       selectTitleClass = 'select-title',
       selectedClass = 'is-selected',
       createSelectContainerClass = 'custom-select',
       createSelectUlClass = 'select-list',
       createOptgroupItemClass = 'select-optgroup',
       createSelectBtnActive = 'active',
-      selectListOpenClass = 'is-open';
+      selectListOpenClass = 'is-open',
+      SelectArray = [];
+
+   // update local storage function
+   const updateLocalStorage = () => {
+      localStorage.setItem('select', JSON.stringify(SelectArray));
+   }
+
+   console.log(selectedIndex);
+
+   // create our object
+   function eventPush() {
+      let selectObject = {
+         selected: selectedIndex
+      };
+      SelectArray.push(selectObject);
+      updateLocalStorage();
+   }
+
+   if (!localStorage.SelectArray) {
+      SelectArray = [];
+   } else {
+      SelectArray = JSON.parse(localStorage.getItem('select'));
+   }
 
    const createSelectContainer = document.createElement('div');
    createSelectContainer.className = createSelectContainerClass;
-   mainInitId.id && (createSelectContainer.id = `custom-${mainInitId.id}`);
+   if (mainInitId.id) createSelectContainer.id = `custom-${mainInitId.id}`;
 
    const createSelectBtn = document.createElement('button');
    createSelectBtn.className = selectTitleClass;
@@ -23,6 +47,19 @@ const CustomSelect = function (e) {
 
    const createSelectUl = document.createElement("ul");
    createSelectUl.className = createSelectUlClass;
+
+   createSelectBtn.addEventListener('mouseup', function () {
+      createSelectUl.classList.toggle(selectListOpenClass);
+      createSelectBtn.classList.toggle(createSelectBtnActive);
+   });
+
+   document.addEventListener('mouseup', function (e) {
+      let isClickInside = createSelectBtn.contains(e.target);
+      if (!isClickInside) {
+         createSelectUl.classList.remove(selectListOpenClass);
+         createSelectBtn.classList.remove(createSelectBtnActive);
+      }
+   })
 
    if (optgroup.length > 0) {
       for (let p = 0; p < optgroup.length; p++) {
@@ -40,52 +77,51 @@ const CustomSelect = function (e) {
          createSelectLi.innerText = e[t].textContent;
          createSelectLi.setAttribute('data-value', e[t].value);
          createSelectLi.setAttribute('data-index', `${dataIndexCount++}`);
+
+         if (selectOption[dataImgCount++].getAttribute('data-srcImg')) {
+            let srcImgIndex = e[t].getAttribute('data-srcImg');
+            const createSelectImg = document.createElement('img');
+            createSelectImg.classList.add('select_img', 'lazy');
+            createSelectImg.setAttribute('src', `${srcImgIndex}`);
+            createSelectLi.appendChild(createSelectImg);
+         }
+
          createSelectUl.appendChild(createSelectLi);
 
-         if (selectOption[mainInitId.selectedIndex].textContent === e[t].textContent) {
+         if (selectedIndex.textContent === e[t].textContent) {
             createSelectLi.classList.add(selectedClass);
-            createSelectBtn.textContent = e[t].textContent;
+            createSelectBtn.innerHTML = e[t].textContent;
          }
       }
    }
+
+   if (selectedIndex.getAttribute('data-srcImg')) {
+      let selectedImg = document.createElement('img');
+      selectedImg.setAttribute('src', `${selectedIndex.getAttribute('data-srcImg')}`);
+      createSelectBtn.appendChild(selectedImg);
+   }
+
+   createSelectContainer.addEventListener('click', e => {
+      const target = e.target;
+
+      if ('LI' === target.tagName) {
+         createSelectContainer.querySelector(`.${selectTitleClass}`).innerHTML = target.innerHTML;
+         mainInitId.options.selectedIndex = target.getAttribute('data-index');
+
+         for (let a = 0; a < selectOption.length; a++) {
+            createSelectUl.querySelectorAll('li')[a].classList.remove(selectedClass);
+            target.classList.add(selectedClass);
+         }
+      }
+      eventPush();
+   });
 
    function assemblyApp() {
       createSelectContainer.appendChild(createSelectBtn);
       createSelectContainer.appendChild(createSelectUl);
-      createSelectContainer.addEventListener('click', e => {
-         e.preventDefault();
-
-         const target = e.target;
-
-         if ('LI' === target.tagName) {
-            createSelectContainer.querySelector(`.${selectTitleClass}`).innerText = target.innerText;
-            mainInitId.options.selectedIndex = target.getAttribute('data-index');
-
-            // const n = s ? new CustomEvent("change", {bubbles: !0}) : new CustomEvent("change");
-            // mainInitId.dispatchEvent(n);
-
-            for (let a = 0; a < selectOption.length; a++) {
-               createSelectUl.querySelectorAll('li')[a].classList.remove(selectedClass);
-               target.classList.add(selectedClass);
-            }
-         }
-      });
       mainInitId.parentNode.insertBefore(createSelectContainer, mainInitId);
       mainInitId.style.display = 'none';
    }
-
-   createSelectBtn.addEventListener('click', function () {
-      createSelectUl.classList.toggle(selectListOpenClass);
-      createSelectBtn.classList.toggle(createSelectBtnActive);
-   });
-
-   document.addEventListener('mouseup', function (e) {
-      let isClickInside = createSelectBtn.contains(e.target);
-      if (!isClickInside) {
-         createSelectUl.classList.remove(selectListOpenClass);
-         createSelectBtn.classList.remove(createSelectBtnActive);
-      }
-   })
 
    return assemblyApp();
 };
